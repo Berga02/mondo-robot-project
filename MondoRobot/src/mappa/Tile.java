3ad20dfce96d84eb2467da0f7c45c2dcea88fff5
berga02 <fabio.berga02@gmail.com>
2023-04-19 19:11:52 +0200

    // Ho iniziato la parte della mappa con la gui

---
+++ MondoRobot/src/mappa/Tile.java
@@ -0,0 +1,17 @@
+package mappa;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+    private BufferedImage image;
+    private boolean collision;
+    
+    public BufferedImage getImage () {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+}
--- MondoRobot/src/mappa/ViewMappa.java
+++ MondoRobot/src/mappa/ViewMappa.java
@@ -1,5 +1,23 @@
 package mappa;
 
 public class ViewMappa {
-    
+    Tile[] tiles;
+    int [][] mapTiles;
+
+
+
+    public void getTileImage() {
+            try {
+                tiles[0] = new Tile();
+                tiles[0].setImage(ImageIO.read(getClass().getResourceAsStream("/components/ground.png")));
+                tiles[1] = new Tile();
+                tiles[1].setImage(ImageIO.read(getClass().getResourceAsStream("/components/wall.png")));
+                tiles[2] = new Tile();
+                tiles[2].setImage(ImageIO.read(getClass().getResourceAsStream("/components/ceil.png")));
+                tiles[3] = new Tile();
+                tiles[3].setImage(ImageIO.read(getClass().getResourceAsStream("/components/water.png")));
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
 }
